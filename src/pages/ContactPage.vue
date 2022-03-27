ad1bc19182baeb103f0cb88698183bc20e624c08
BuildTools <justindimichele@gmail.com>
2022-03-27 16:07:15 -0700

    Finished copying over the old content from the ContactPage, added an embedded Google Map link to the fire station, and added additional styles.

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,26 +1,53 @@
 <template>
   <div class="componentCard">
-    <h1 class="font-bold underline text-5xl text-red-600 text-center">
-      In An Emergency:
-    </h1>
-    <h1 class="font-bold underline text-5xl text-red-600 text-center">
-      CALL 9-1-1
-    </h1>
-    
-    <div>
+    <div class="font-bold underline text-5xl text-red-600 text-center">
+      <h1>In An Emergency:</h1>
+      <h1>CALL 9-1-1</h1>
+    </div>
+    <div class="pb-8">
       <ul>
-        <li>
-          Non-emergency phone: 607-272-8114 (unmanned station; will usually go
-          to voice mail)
+        <li class="font-bold flex-grow border-b-2 ml-5 mr-10 border-danby-red">
+          Non-emergency phone:
         </li>
-        <li>
-          Fire Station: Danby Volunteer Fire Company 1780 Danby Rd Ithaca NY
-          14850-9418
+        <li class="indent-10">
+          <a href="tel:607-272-8114" class="hover:underline text-blue-700"
+            >607-272-8114</a
+          >
+          (unmanned station; will usually go to voice mail)
         </li>
-        <li>
-          Email: Webmaster: Simon Wyatt swyatt@danbyfiredistrict.org Requests
-          for station use: danbyfirestation@gmail.com
+      </ul>
+    </div>
+    <div class="pb-8">
+      <ul>
+        <li class="font-bold flex-grow border-b-2 ml-5 mr-10 border-danby-red">
+          Fire Station:
         </li>
+        <li class="indent-10">
+          <a
+            href="https://goo.gl/maps/zTgiQ5hUkAfMYj617"
+            target="_blank"
+            class="hover:underline text-blue-700"
+            >1780 Danby Rd, Ithaca, NY 14850</a
+          >
+        </li>
+        <iframe
+          class="w-96 h-96 border-0 ml-10"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d737.0807780632854!2d-76.48332497976094!3d42.356952791620415!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xeecec980c3cbe59f!2sDanby%20Fire%20Station!5e0!3m2!1sen!2sus!4v1648421720564!5m2!1sen!2sus"
+          allowfullscreen="true"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </ul>
+    </div>
+    <div class="pb-8">
+      <ul>
+        <li class="font-bold flex-grow border-b-2 ml-5 mr-10 border-danby-red">
+          Email:
+        </li>
+      </ul>
+      <ul class="indent-10">
+        <li><b>Webmaster:</b> Simon Wyatt swyatt@danbyfiredistrict.org</li>
+        <li><b>Requests for station use:</b> danbyfirestation@gmail.com</li>
       </ul>
     </div>
   </div>
