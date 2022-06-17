263fb77fc7a72a63f907a477e9729ceb515ef40e
BuildTools <justindimichele@gmail.com>
2022-06-17 13:15:12 -0700

    Modified the routing page to include the BASE_URL environment node to help with the 404 issue when deployed to the server.

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,63 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../pages/HomePage.vue';
-import AboutPage from '../pages/AboutPage.vue';
-import SafetyInfoPage from '../pages/SafetyInfoPage.vue';
-import VolunteerPage from '../pages/VolunteerPage.vue';
-import LinksPage from '../pages/LinksPage.vue';
-import ContactPage from '../pages/ContactPage.vue';
-import TrainingPage from '../pages/TrainingPage.vue';
-import NotFound from '../components/interactive/NotFound.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import AboutPage from "../pages/AboutPage.vue";
+import SafetyInfoPage from "../pages/SafetyInfoPage.vue";
+import VolunteerPage from "../pages/VolunteerPage.vue";
+import LinksPage from "../pages/LinksPage.vue";
+import ContactPage from "../pages/ContactPage.vue";
+import TrainingPage from "../pages/TrainingPage.vue";
+import NotFound from "../components/interactive/NotFound.vue";
 
 const routes = [
-  { path: '/', redirect: '/home'},
   {
-    path: '/home',
-    name: 'home',
-    component: HomePage
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/about',
-    name: 'about',
-    component: AboutPage
+    path: "/home",
+    name: "home",
+    component: HomePage,
   },
   {
-    path: '/safety',
-    name: 'safety',
-    component: SafetyInfoPage
+    path: "/about",
+    name: "about",
+    component: AboutPage,
   },
   {
-    path: '/volunteer',
-    name: 'volunteer',
-    component: VolunteerPage
+    path: "/safety",
+    name: "safety",
+    component: SafetyInfoPage,
   },
   {
-    path: '/links',
-    name: 'links',
-    component: LinksPage
+    path: "/volunteer",
+    name: "volunteer",
+    component: VolunteerPage,
   },
   {
-    path: '/contact',
-    name: 'contact',
-    component: ContactPage
+    path: "/links",
+    name: "links",
+    component: LinksPage,
   },
   {
-    path: '/training',
-    name: 'training',
-    component: TrainingPage
+    path: "/contact",
+    name: "contact",
+    component: ContactPage,
   },
-  { path: '/:notFound(.*)', component: NotFound},
-]
+  {
+    path: "/training",
+    name: "training",
+    component: TrainingPage,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    component: NotFound,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
