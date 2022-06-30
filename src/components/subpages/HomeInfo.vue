6129941e4c4a3d12950b491e4e222cf2c4a1af9d
BuildTools <justindimichele@gmail.com>
2022-06-29 20:40:19 -0700

    Adding Timeline and Events to Facebook iframe.

--- src/components/subpages/HomeInfo.vue
+++ src/components/subpages/HomeInfo.vue
@@ -8,6 +8,7 @@
         src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FDanbyVolunteerFireCompany%2F&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
         scrolling="no"
         frameborder="0"
+        tabs="timeline, events"
         allowfullscreen="true"
         allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
       ></iframe>
