a8151e6bacf131b9c84ae075a3362814915d7c61
jdimichele <justindimichele@gmail.com>
2023-12-27 18:39:40 -0800

    Additional info and pricing added to the form when choosing between PayPal or check payments.

--- src/components/subpages/HouseSignForm.vue
+++ src/components/subpages/HouseSignForm.vue
@@ -301,15 +301,18 @@
             </div>
           </div>
           <div v-if="paymentOption === 'PayPal'">
-            <p></p>
+            <div class="text-base ml-10">
+              <h1>Please send click the link below to proceed with your PayPal payment:</h1>
+              <h1>Total amount: ${{ totalPrice }}</h1>
+            </div>
           </div>
           <div v-else-if="paymentOption === 'Check'">
-            <p class="text-base">
-              Please send payment with the following information: Payment to:
-              Danby Volunteer Fire Company. Attn: House Signs Total amount: ${{
-                totalPrice
-              }}
-            </p>
+            <div class="text-base ml-10">
+              <h1>Please send payment with the following information:</h1>
+              <h1>Payment to: Danby Volunteer Fire Company.</h1>
+              <h1>Attn: House Signs</h1>
+              <h1>Total amount: ${{ totalPrice }}</h1>
+            </div>
           </div>
         </div>
 
@@ -334,48 +337,50 @@
     </div>
   </div>
 
-  <div
-    v-if="confirmation"
-    class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative text-center mb-20"
-    role="alert"
-  >
-    <span class="block sm:inline">{{ confirmation }}</span>
-    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-      <svg
-        @click="confirmation = null"
-        class="fill-current h-6 w-6 text-green-500"
-        role="button"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <title>Close</title>
-        <path
-          d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
-        />
-      </svg>
-    </span>
-  </div>
+  <div class="py-1">
+    <div
+      v-if="confirmation"
+      class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative text-center mb-20"
+      role="alert"
+    >
+      <span class="block sm:inline">{{ confirmation }}</span>
+      <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+        <svg
+          @click="confirmation = null"
+          class="fill-current h-6 w-6 text-green-500"
+          role="button"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <title>Close</title>
+          <path
+            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+          />
+        </svg>
+      </span>
+    </div>
 
-  <div
-    v-if="error"
-    class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center"
-    role="alert"
-  >
-    <span class="block sm:inline">{{ error }}</span>
-    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-      <svg
-        @click="error = null"
-        class="fill-current h-6 w-6 text-red-500"
-        role="button"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <title>Close</title>
-        <path
-          d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
-        />
-      </svg>
-    </span>
+    <div
+      v-if="error"
+      class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative text-center"
+      role="alert"
+    >
+      <span class="block sm:inline">{{ error }}</span>
+      <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+        <svg
+          @click="error = null"
+          class="fill-current h-6 w-6 text-red-500"
+          role="button"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <title>Close</title>
+          <path
+            d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+          />
+        </svg>
+      </span>
+    </div>
   </div>
 </template>
 
